8d6aab45c92d781c7000d328716cc6db80c5e62d
Eseteselase <esete.s.berhanu@gmail.com>
2023-11-13 22:23:32 +0300

    including environ

--- execute_function.c
+++ execute_function.c
@@ -1,5 +1,5 @@
 #include"header_file.h"
-
+extern char **environ;
 void execute_fun(const char *input)
 {
 	pid_t child_process_id = fork ();
@@ -14,15 +14,16 @@ void execute_fun(const char *input)
 		if (cmd == NULL)
 		{
 			perror("malloc");
+                        exit(1);
+		}
+        	cmd[0] = (char *)input;
+		cmd[1] = NULL;
+		if (execve(input, cmd, environ) == -1)
+		{
+			perror("./shell");
+			free(cmd);
 			exit(1);
 		}
-		cmd[0] = (char *)input;
-		cmd[1] = NULL;
-
-		execve(input, cmd, NULL);
-		perror("./shell");
-		free(cmd);
-		exit(1);
 	}
 	else
 		wait (NULL);
