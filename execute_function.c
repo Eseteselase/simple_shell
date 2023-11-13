1727e1d4a64feb51ab02ad13fee35c8be24b96d4
Eseteselase <esete.s.berhanu@gmail.com>
2023-11-13 21:04:54 +0300

    1.Simple shell 0.1

--- execute_function.c
+++ execute_function.c
@@ -9,12 +9,20 @@ void execute_fun(const char *input)
 		exit(1);
 	}
 	if (child_process_id == 0)
-	{		
-		if (execve(input, (char *const *)(&input), NULL) == -1)
+	{
+		char **cmd = malloc(2 * sizeof(char *));
+		if (cmd == NULL)
 		{
-			perror("./shell");			
+			perror("malloc");
 			exit(1);
 		}
+		cmd[0] = (char *)input;
+		cmd[1] = NULL;
+
+		execve(input, cmd, NULL);
+		perror("./shell");
+		free(cmd);
+		exit(1);
 	}
 	else
 		wait (NULL);
