55501de27f0600156ed6cc87b9bc34d399053163
Eseteselase <esete.s.berhanu@gmail.com>
2023-11-14 11:10:59 +0300

    update

--- simple_shell.c
+++ simple_shell.c
@@ -2,23 +2,24 @@
 
 void input_fun(char **input, size_t *length)
 {
-        ssize_t read_result;
-        read_result = getline(input, length, stdin);
-        if (read_result == -1)
-        {
-                if (feof(stdin))
-                {
-                        print_fun("\n");
-                        exit(0);
-                }
-                else
-                {
-                        print_fun("Error Input\n");
-                        exit(1);
-                }
-        }
-        if ((*input)[read_result - 1] == '\n')
-                (*input)[read_result - 1] = '\0';
+	ssize_t read_result;
+	read_result = getline(input, length, stdin);
+	
+	if (read_result == -1)
+	{
+		if (feof(stdin))
+		{
+			print_fun("\n");
+			exit(0);
+		}
+		else
+		{
+			print_fun("Error Input\n");
+			exit(1);
+		}
+	}
+	if ((*input)[read_result - 1] == '\n')
+		(*input)[read_result - 1] = '\0';
 }
 
 extern char **environ;
