c0531813e004da680dd2f498b40f90af872e7abd
Eseteselase <esete.s.berhanu@gmail.com>
2023-11-14 11:17:38 +0300

    update

--- simple_shell.c
+++ simple_shell.c
@@ -25,40 +25,40 @@ void input_fun(char **input, size_t *length)
 extern char **environ;
 void execute_fun(const char *input)
 {
-        pid_t child_process_id = fork();
+	pid_t child_process_id = fork();
 
-        if (child_process_id == -1)
-        {
-                perror("fork");
-                exit(1);
-        }
-        if (child_process_id == 0)
-        {
-                char **cmd = malloc(2 * sizeof(char *));
+	if (child_process_id == -1)
+	{
+		perror("fork");
+		exit(1);
+	}
+	if (child_process_id == 0)
+	{
+		char **cmd = malloc(2 * sizeof(char *));
 
-                if (cmd == NULL)
-                {
-                        perror("malloc");
-                        exit(1);
-                }
-                cmd[0] = (char *)input;
-                cmd[1] = NULL;
-                if (execve(input, cmd, environ) == -1)
-                {
-                        perror("./shell");
-                        free(cmd);
-                        exit(1);
-                }
-        }
-        else
-                wait(NULL);
+		if (cmd == NULL)
+		{
+			perror("malloc");
+			exit(1);
+		}
+		cmd[0] = (char *)input;
+		cmd[1] = NULL;
+		if (execve(input, cmd, environ) == -1)
+		{
+			perror("./shell");
+			free(cmd);
+			exit(1);
+		}
+	}
+	else
+		wait(NULL);
 }
 
 int main(void)
 {
 	char *input = NULL;
 	size_t length =0;
-
+	
 	while (1)
 	{
 		display_prompt_fun();
