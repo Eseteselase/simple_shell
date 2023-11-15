751f0c9e32662a10cfcbd0d5464cfe79d9061f17
Eseteselase <esete.s.berhanu@gmail.com>
2023-11-15 12:18:46 -0800

    path

--- shell.h
+++ shell.h
@@ -13,6 +13,7 @@ void display_prompt_fun(void);
 void input_fun(char **input, size_t *length);
 char **tokenize_input(char *input);
 void trim_newline(char *str);
+char *find_command_path(const char *command);
 void execute_fun(char *input);
 
 #endif
