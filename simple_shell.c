b9e237b0bc0fb4c22999a946fb04dd95d74d5692
Eseteselase <esete.s.berhanu@gmail.com>
2023-11-15 06:41:48 -0800

    ls

--- simple_shell.c
+++ simple_shell.c
@@ -4,33 +4,39 @@
  * main - maonks
  * Return: 0
  */
-int main()
+
+int main(void)
 {
-        char command[100];
-        while (1)
-        {
-                pid_t child_pid;
+	char command[100];
+
+	while (1)
+	{
+		pid_t child_pid;
+
 		printf("$ ");
-                if (fgets(command, sizeof(command), stdin) == NULL)
-                {
-                        printf("\n");
-                        break;
-                }
-                command[strlen(command) - 1] = '\0';
-                if (command[0] == 3)
-                {
-                        printf("\n");
-                        break;
-                }
-                child_pid = fork();
-                if (child_pid == -1)
-                {
-                        perror("fork");
-                        exit(1);
-                }
-                if (child_pid == 0)
-                {
+
+		if (fgets(command, sizeof(command), stdin) == NULL)
+		{
+			printf("\n");
+			break;
+		}
+		command[strlen(command) - 1] = '\0';
+		if (command[0] == 3)
+		{
+			printf("\n");
+			break;
+		}
+		child_pid = fork();
+
+		if (child_pid == -1)
+		{
+			perror("fork");
+			exit(1);
+		}
+		if (child_pid == 0)
+		{
 			char **cmd = malloc(sizeof(char *) * 2);
+
 			if (cmd == NULL)
 			{
 				perror("malloc");
@@ -38,14 +44,13 @@ int main()
 			}
 			cmd[0] = command;
 			cmd[1] = NULL;
-
 			execvp(cmd[0], cmd);
-                        perror("execve");
+			perror("execve");
 			free(cmd);
-                        exit(1);
-                }
-                else
-                        wait(NULL);
-        }
-        return 0;
+			exit(1);
+		}
+		else
+			wait(NULL);
+	}
+	return 0;
 }
