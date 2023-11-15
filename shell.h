bbbd54b84f36ed99c1baf74ebbe4735f2cc2e73b
Eseteselase <esete.s.berhanu@gmail.com>
2023-11-15 09:49:40 +0300

    01

--- shell.h
+++ shell.h
@@ -13,8 +13,5 @@ extern char **environ;
 void print_fun(const char *input);
 void display_prompt_fun(void);
 void input_fun(char **input, size_t *length);
-void custom_itoa(int value, char *str);
-void custom_strcat(char *dest, const char *src);
-void getenv_fun(char *new_path);
 
 #endif
